4907e56d081c0e93a9e5ebdbacf89a7b5bc90923
martin <meatole.cs@gmail.com>
2025-11-03 02:55:35 +0800

    feat: Replace sun light and glowing effects in TriumphModel with a Moon component for enhanced visual representation

--- src/features/3d-model/components/triumph-model.tsx
+++ src/features/3d-model/components/triumph-model.tsx
@@ -11,6 +11,7 @@ import { GLTF } from 'three-stdlib'
 import Phonograph from './objects/phonograph'
 import Lamp from './objects/lamp'
 import Chair from './objects/chair'
+import Moon from './objects/moon'
 
 type TriumphModelProps = GLTF & {
     nodes: {
@@ -272,52 +273,7 @@ export function TriumphModel(props?: Partial<TriumphModelProps>) {
                     material={materials.SideWindow}
                 />
 
-                {/* Sun outside the window - positioned in model space */}
-                <group position={[-40, 15, -70]}>
-                    {/* Sun light source */}
-                    <pointLight
-                        castShadow
-                        position={[15, 0, 5]}
-                        intensity={8000}
-                        color="white"
-                        distance={200}
-                        decay={1.9}
-                    />
-
-                    {/* Visible sun circle with glowing layers */}
-                    {/* Core sun sphere */}
-                    <mesh>
-                        <sphereGeometry args={[5, 32, 32]} />
-                        <meshStandardMaterial
-                            color="white"
-                            emissive="white"
-                            emissiveIntensity={1}
-                        />
-                    </mesh>
-                    {/* Inner glow layer */}
-                    <mesh>
-                        <sphereGeometry args={[7, 32, 32]} />
-                        <meshStandardMaterial
-                            color="#fff9e6"
-                            emissive="#fff9e6"
-                            emissiveIntensity={1}
-                            transparent
-                            opacity={0.6}
-                        />
-                    </mesh>
-                    {/* Outer glow/halo */}
-                    <mesh>
-                        <sphereGeometry args={[9, 32, 32]} />
-                        <meshStandardMaterial
-                            color="#fff5cc"
-                            emissive="#fff5cc"
-                            emissiveIntensity={0.5}
-                            transparent
-                            opacity={0.3}
-                        />
-                    </mesh>
-
-                </group>
+                <Moon />
             </group>
         </group>
     )
