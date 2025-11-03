e5083c213a83d67c2780db5d6a67c0b306305613
martin <meatole.cs@gmail.com>
2025-11-03 22:58:14 +0800

    feat: Enhance event page functionality with responsive layout adjustments, dynamic sizing based on screen size, and updated event titles and descriptions for improved user experience

--- src/app/(public)/events/page.tsx
+++ src/app/(public)/events/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { motion, useTransform, useScroll } from "motion/react"
+import { motion, useTransform, useScroll, MotionValue } from "motion/react"
 import Image from "next/image"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import Link from "next/link"
 import { ArrowUpRight } from "lucide-react"
 import { Beau_Rivage } from "next/font/google"
@@ -17,12 +17,11 @@ const beau_rivage = Beau_Rivage({
 const events = [
     {
         id: 1,
-        title: "Event 1",
-        description: "Description 1",
+        title: "Page 1",
         feature: {
             image: "/events/plant1.webp",
             title: "Plant 1",
-            className: "absolute left-1/2 -translate-x-1/2 -bottom-60 w-full h-full"
+            className: "absolute left-1/2 -translate-x-1/2 -bottom-60 w-full h-[85%] 2xl:w-full 2xl:h-full"
         },
         eventStatues: [
             {
@@ -32,7 +31,7 @@ const events = [
                 description: 'Ateneo vibrant welcome tradition. Where freshies are introduced to campus life, Atenean culture, and the spirit of community through cheers, performances, and unforgettable memories.',
                 imageClassName: "h-[40%]",
                 titleClassName: "",
-                containerClassName: "2xl:mt-20"
+                containerClassName: "mt-20 2xl:mt-20"
             },
             {
                 image: "/events/frame2.webp",
@@ -41,108 +40,137 @@ const events = [
                 description: `The feast of our founder, St. Ignatius of Loyola. A day of faith, fun, and community, filled with Masses, performances, and Ignatian spirit all around campus.`,
                 imageClassName: "h-[55%]",
                 titleClassName: "",
-                containerClassName: "2xl:mt-45"
+                containerClassName: "mt-40 2xl:mt-45"
             },
             {
                 image: "/events/frame3.webp",
                 title: "Mass of the Holy Spirit",
                 link: "/events/3",
                 description: `Coming together as one Ateneo community to pray for wisdom, courage, and grace for the months ahead.`,
-                imageClassName: "h-[40%]",
+                imageClassName: "h-[50%]",
+                titleClassName: "",
+                containerClassName: "mt-2 2xl:mt-10"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Page 2",
+        feature: {
+            image: "/events/lamp.webp",
+            title: "Lamp 2",
+            className: "absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/3 w-full 2xl:w-[60%] h-1/2 object-cover"
+        },
+        eventStatues: [
+            {
+                image: "/events/frame4.webp",
+                title: "Traslacion",
+                link: "/events/4",
+                description: "Millions of devotees join the procession, many barefoot, as an act of penitence, thanksgiving, and devotion.",
+                imageClassName: "h-[50%]",
+                titleClassName: "",
+                containerClassName: "2xl:mt-30"
+            },
+            {
+                image: "/events/frame5.webp",
+                title: "Peñafrancia Fiesta",
+                link: "/events/5",
+                description: "Viva la Virgen! The Peñafrancia Fiesta unites Bicolanos and devotees from all over in a vibrant celebration of faith, culture, and devotion to Our Lady of Peñafrancia.",
+                imageClassName: "h-[50%]",
+                titleClassName: "",
+                containerClassName: "2xl:mt-50"
+            },
+            {
+                image: "/events/frame6.webp",
+                title: "Alternative Class Program",
+                link: "/events/6",
+                description: "Takes learning outside the classroom—through talks, workshops, and experiences that open minds, spark conversations, and inspire action.",
+                imageClassName: "h-[50%]",
                 titleClassName: "",
                 containerClassName: "2xl:mt-10"
             }
         ]
     },
     {
-        id: 2,
-        title: "Event 2",
-        description: "Description 2",
-        feature: {
-            image: "/events/lamp.webp",
-            title: "Lamp 2",
-            className: "absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/3 w-[60%] h-1/2 object-cover"
-        },
-        eventStatues: [
-            {
-                image: "/images/event-statue-4.jpg",
-                title: "Event Statue 4",
-                link: "/events/4",
-                description: 'A symbol of resilience and hope, the Lamplighter statue commemorates the sacrifices made by the fallen during World War I. Its presence reminds us of the importance of remembering and honoring those who have served and sacrificed for our freedom.',
-                imageClassName: "",
-                titleClassName: "",
-                containerClassName: "justify-start"
-            },
-            {
-                image: "/images/event-statue-5.jpg",
-                title: "Event Statue 5",
-                link: "/events/5",
-                description: 'A symbol of resilience and hope, the Lamplighter statue commemorates the sacrifices made by the fallen during World War I. Its presence reminds us of the importance of remembering and honoring those who have served and sacrificed for our freedom.',
-                imageClassName: "",
-                titleClassName: "",
-                containerClassName: "justify-end"
-            },
-            {
-                image: "/images/event-statue-6.jpg",
-                title: "Event Statue 6",
-                link: "/events/6",
-                description: 'A symbol of resilience and hope, the Lamplighter statue commemorates the sacrifices made by the fallen during World War I. Its presence reminds us of the importance of remembering and honoring those who have served and sacrificed for our freedom.',
-                imageClassName: "",
-                titleClassName: "",
-                containerClassName: "justify-start"
-            }
-        ]
-    },
-    {
         id: 3,
-        title: "Event 3",
-        description: "Description 3",
+        title: "Page 3",
         feature: {
-            image: "/images/style-statue-3.jpg",
-            title: "Style Statue 3"
+            image: "/events/plant2.webp",
+            title: "Plant 2",
+            className: "absolute left-1/2 -translate-x-1/2 -bottom-60 w-full h-full"
         },
         eventStatues: [
             {
-                image: "/images/event-statue-7.jpg",
-                title: "Event Statue 7",
+                image: "/events/frame7.webp",
+                title: "St. Francis Xavier Day",
                 link: "/events/7",
-                description: 'A symbol of resilience and hope, the Lamplighter statue commemorates the sacrifices made by the fallen during World War I. Its presence reminds us of the importance of remembering and honoring those who have served and sacrificed for our freedom.',
-                imageClassName: "",
+                description: "Honoring the Jesuit missionary spirit of courage, faith, and service that continues to inspire Ateneans today.",
+                imageClassName: "h-[50%]",
                 titleClassName: "",
-                containerClassName: "justify-start"
+                containerClassName: "2xl:mt-50"
             },
             {
-                image: "/images/event-statue-8.jpg",
-                title: "Event Statue 8",
+                image: "/events/frame8.webp",
+                title: "Orgs Fest",
                 link: "/events/8",
-                description: 'A symbol of resilience and hope, the Lamplighter statue commemorates the sacrifices made by the fallen during World War I. Its presence reminds us of the importance of remembering and honoring those who have served and sacrificed for our freedom.',
-                imageClassName: "",
+                description: "Where orgs bring the campus to life with booths, games, and performances, all while welcoming new members into their families.",
+                imageClassName: "h-[50%]",
                 titleClassName: "",
-                containerClassName: "justify-end"
+                containerClassName: "2xl:mt-10"
             },
             {
-                image: "/images/event-statue-9.jpg",
-                title: "Event Statue 9",
+                image: "/events/frame9.webp",
+                title: "Dugong Atenista",
                 link: "/events/9",
-                description: 'A symbol of resilience and hope, the Lamplighter statue commemorates the sacrifices made by the fallen during World War I. Its presence reminds us of the importance of remembering and honoring those who have served and sacrificed for our freedom.',
-                imageClassName: "",
+                description: "Ateneo’s annual bloodletting activity that embodies the spirit of generosity and solidarity. Through this initiative, students, faculty, and staff come together to donate blood and help save lives—living out the Atenean value of being men and women for others.",
+                imageClassName: "h-[50%]",
                 titleClassName: "",
-                containerClassName: "justify-start"
+                containerClassName: "2xl:mt-40"
             }
         ]
     },
 ]
 
 export default function EventsPage() {
+    const [screenSize, setScreenSize] = useState<number | null>(null)
     const targetRef = useRef<HTMLDivElement>(null)
 
     const { scrollYProgress } = useScroll({
         target: targetRef,
     })
 
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenSize(window.innerWidth)
+        }
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+
     // Transform horizontal position based on scroll progress
     // Adjust based on number of events: -(numberOfEvents - 1) * 100
-    const x = useTransform(scrollYProgress, [0, 1], ['0%', `-${(events.length - 1) * 100}%`])
+
+
+    let x: MotionValue<string>;
+
+    switch (screenSize) {
+        case (screenSize && screenSize < 768): // Mobile
+            x = useTransform(scrollYProgress, [0, 1], ['0%', `-${(events.length - 1) * 195}%`])
+            break;
+        case (screenSize && screenSize >= 768): // Tablet
+            x = useTransform(scrollYProgress, [0, 1], ['0%', `-${(events.length - 1) * 100}%`])
+            break;
+        case (screenSize && screenSize >= 1024): // Small Laptop
+            x = useTransform(scrollYProgress, [0, 1], ['0%', `-${(events.length - 1) * 100}%`])
+            break;
+        case (screenSize && screenSize >= 1280): // Default Laptop
+            x = useTransform(scrollYProgress, [0, 1], ['0%', `-${(events.length - 1) * 100}%`])
+            break;
+        default: // Large Desktop
+            x = useTransform(scrollYProgress, [0, 1], ['0%', `-${(events.length - 1) * 100}%`])
+            break;
+    }
 
     return (
         <main className="relative h-[400vh]">
@@ -187,11 +215,10 @@ type EventPageProps = {
 
 const EventPage = ({ event }: EventPageProps) => {
     return (
-        <div className="shrink-0 w-screen h-screen flex items-center justify-center">
-            {/* Grid Layout: 1 style statue + 3 event statues */}
-            <div className="grid grid-cols-4 w-full h-full max-h-screen z-20 relative">
+        <div className="shrink-0 w-[140%] 2xl:w-full h-screen flex items-center justify-center">
+            <div className="flex flex-row gap-x-3 w-full h-full min-h-screen z-20 relative">
                 {/* Style Statue - Takes 1 column, spans 2 rows */}
-                <div className="relative w-full h-full">
+                <div className="relative w-[80%] 2xl:w-full h-full">
                     <div className={event.feature.className}>
                         <Image
                             src={event.feature.image}
@@ -203,10 +230,8 @@ const EventPage = ({ event }: EventPageProps) => {
                 </div>
                 {/* Event Statues - 3 items in 1 column */}
                 {event.eventStatues.map((statue, index) => {
-
-
                     return (
-                        <div key={index} className={`relative w-full h-full flex flex-col ${statue.containerClassName || ''}`}>
+                        <div key={index} className={`relative w-full 2xl:w-full h-full flex flex-col ${statue.containerClassName || ''}`}>
                             {/* Picture Frame Container - Allow dynamic sizing */}
                             <div className={`relative w-full ${statue.imageClassName || 'h-1/2'} shrink-0`}>
                                 <Image
@@ -219,7 +244,7 @@ const EventPage = ({ event }: EventPageProps) => {
 
                             {/* Title and Description - Positioned right below the image container */}
                             <div className={`pb-4 px-4 md:pb-6 md:px-6 flex flex-col justify-start items-center flex-1 min-h-0 ${statue.titleClassName || ''}`}>
-                                <h3 className={`text-lg md:text-7xl font-bold text-[#F4E590] mb-1 md:mb-2 ${beau_rivage.variable} ${beau_rivage.className}`}>
+                                <h3 className={`text-lg md:text-6xl text-center font-bold text-[#F4E590] mb-1 md:mb-2 ${beau_rivage.variable} ${beau_rivage.className}`}>
                                     {statue.title}
                                 </h3>
                                 <p className="text-sm md:text-base text-white text-center">
